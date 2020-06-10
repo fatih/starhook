d54c5d10e10dac1c6fcd164e09dbbcd785bf3790
Fatih Arslan <ftharsln@gmail.com>
2020-06-10 00:33:07 -0700

    Add tests for FetchRepos

--- internal/gh/gh_test.go
+++ internal/gh/gh_test.go
@@ -18,24 +18,60 @@ func TestNewClient(t *testing.T) {
 func TestClient_FetchRepos(t *testing.T) {
 	ctx := context.Background()
 
+	repoName1 := "foo"
+	repoName2 := "bar"
+
+	searchService := &mockSearchService{
+		RepositoriesFunc: func(ctx context.Context, query string, opt *github.SearchOptions) (*github.RepositoriesSearchResult, *github.Response, error) {
+			return &github.RepositoriesSearchResult{
+				Repositories: []github.Repository{
+					{
+						Name: &repoName1,
+					},
+					{
+
+						Name: &repoName2,
+					},
+				},
+			}, &github.Response{}, nil
+		},
+	}
+
+	client := &Client{
+		Search: searchService,
+	}
+
+	query := "org:github language:go"
+
+	repos, err := client.FetchRepos(ctx, query)
+	testutil.Ok(t, err)
+	testutil.Assert(t, searchService.RepositoriesInvoked, "Repositories() should be called")
+	testutil.Equals(t, len(repos), 2)
+}
+
+func TestClient_Branch(t *testing.T) {
+	ctx := context.Background()
+
 	wantSHA := "123"
 	updatedAt := time.Now()
 
-	client := &Client{
-		Repositories: &mockRepositoriesService{
-			GetBranchFunc: func(ctx context.Context, owner, repo, branch string) (*github.Branch, *github.Response, error) {
-				return &github.Branch{
-					Commit: &github.RepositoryCommit{
-						SHA: &wantSHA,
-						Commit: &github.Commit{
-							Committer: &github.CommitAuthor{
-								Date: &updatedAt,
-							},
+	repoService := &mockRepositoriesService{
+		GetBranchFunc: func(ctx context.Context, owner, repo, branch string) (*github.Branch, *github.Response, error) {
+			return &github.Branch{
+				Commit: &github.RepositoryCommit{
+					SHA: &wantSHA,
+					Commit: &github.Commit{
+						Committer: &github.CommitAuthor{
+							Date: &updatedAt,
 						},
 					},
-				}, nil, nil
-			},
+				},
+			}, nil, nil
 		},
+	}
+
+	client := &Client{
+		Repositories: repoService,
 	}
 
 	owner := "fatih"
@@ -46,6 +82,7 @@ func TestClient_FetchRepos(t *testing.T) {
 	testutil.Ok(t, err)
 	testutil.Equals(t, resp.SHA, wantSHA)
 	testutil.Equals(t, resp.UpdatedAt, updatedAt)
+	testutil.Assert(t, repoService.GetBranchInvoked, "GetBranch() should be called")
 }
 
 type mockRepositoriesService struct {
@@ -54,8 +91,22 @@ type mockRepositoriesService struct {
 }
 
 func (m *mockRepositoriesService) GetBranch(ctx context.Context, owner, repo, branch string) (*github.Branch, *github.Response, error) {
+	m.GetBranchInvoked = true
 	if m.GetBranchFunc != nil {
 		return m.GetBranchFunc(ctx, owner, repo, branch)
+	}
+	return nil, &github.Response{}, nil
+}
+
+type mockSearchService struct {
+	RepositoriesFunc    func(ctx context.Context, query string, opt *github.SearchOptions) (*github.RepositoriesSearchResult, *github.Response, error)
+	RepositoriesInvoked bool
+}
+
+func (m *mockSearchService) Repositories(ctx context.Context, query string, opt *github.SearchOptions) (*github.RepositoriesSearchResult, *github.Response, error) {
+	m.RepositoriesInvoked = true
+	if m.RepositoriesFunc != nil {
+		return m.RepositoriesFunc(ctx, query, opt)
 	}
 	return nil, &github.Response{}, nil
 }
